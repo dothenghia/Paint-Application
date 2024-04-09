01f3431b1a9c138d447666b297ccddd79e5fef89
Thế Nghĩa <nghiask2018@gmail.com>
2024-04-09 18:01:32 +0700

    feat: Setup project structure

---
+++ Paint-Application/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Paint_Application
+{
+    public partial class MainWindow : Window
+    {
+        public Paint paint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Paint-Application/Paint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Paint_Application
+{
+    public class Paint
+    {
+        public Canvas mainCanvas;
+
+        public Paint(Canvas mainCanvas)
+        {
+            this.mainCanvas = mainCanvas;
+        }
+
+
+
+
+
+
+
+
+    }
+}
