2a1c32b056127d0b40c0a031a584ec2c1e205b28
Thế Nghĩa <nghiask2018@gmail.com>
2024-04-10 13:22:31 +0700

    feat: Draw Line

--- Paint-Application/MainWindow.xaml.cs
+++ Paint-Application/MainWindow.xaml.cs
@@ -21,31 +21,29 @@ namespace Paint_Application
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e) 
         {
             paint = new Paint(Main_Canvas);
         }
 
+        // ==================== Tool Bar Handlers ====================
         private void OpenButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Open File"); }
         private void SaveButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Save File"); }
         private void UndoButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Undo"); }
         private void RedoButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Redo"); }
         private void ExitButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Exit"); }
 
+
+
+        // ==================== Functional Bar Handlers ====================
         private void SelectButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Select"); }
 
-        private void DrawLineButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Draw Line"); }
-        private void DrawRectangleButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Draw Rectangle"); }
-        private void DrawEllipseButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Draw Ellipse"); }
-        private void DrawTriangleButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Draw Triangle"); }
+        private void DrawLineButton_Click(object sender, RoutedEventArgs e) { paint.currentShape = Paint.MyShape.Line; }
+        private void DrawRectangleButton_Click(object sender, RoutedEventArgs e) { paint.currentShape = Paint.MyShape.Rectangle; }
+        private void DrawEllipseButton_Click(object sender, RoutedEventArgs e) { paint.currentShape = Paint.MyShape.Ellipse; }
+        private void DrawTriangleButton_Click(object sender, RoutedEventArgs e) { paint.currentShape = Paint.MyShape.Triangle; }
 
-        private void PaintColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
-        {
-            if (e.NewValue.HasValue)
-            {
-                Debug.WriteLine("Color Changed");
-            }
-        }
+        private void PaintColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e) { }
 
         private void SizeButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Change Size"); }
         private void StrokeButton_Click(object sender, RoutedEventArgs e) { Debug.WriteLine("Change Stroke"); }
@@ -53,6 +51,27 @@ namespace Paint_Application
 
 
 
+        // ==================== Main Canvas Handlers ====================
+        private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            paint.startPoint = e.GetPosition(Main_Canvas);
+            paint.endPoint = e.GetPosition(Main_Canvas);
+            paint.StartShape();
+        }
+
+        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                paint.endPoint = e.GetPosition(Main_Canvas);
+                paint.UpdateShape();
+            }
+        }
+
+        private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            paint.EndShape();
+        }
 
 
 
