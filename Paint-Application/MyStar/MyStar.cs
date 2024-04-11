d02779488f54e5a1b8f71a5fd0ee928b88a228a4
Thế Nghĩa <nghiask2018@gmail.com>
2024-04-11 21:01:01 +0700

    feat: Design external DLL files

---
+++ Paint-Application/MyStar/MyStar.cs
@@ -0,0 +1,49 @@
+
+using MyShapes;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace MyStar
+{
+    public class MyStar : IShape
+    {
+        // ==================== Attributes ====================
+        public string Name => "Star"; // Name of the shape
+        public string Icon => "Assets/star.png"; // Path to the icon
+        public Point startPoint; // Start point of the shape
+        public Point endPoint; // End point of the shape
+
+
+        // ==================== Methods ====================
+        public void SetStartPoint(Point point)
+        {
+            startPoint = point;
+        }
+        public void SetEndPoint(Point point)
+        {
+            endPoint = point;
+        }
+
+        // Clone the object
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        // Convert the object to a UIElement - Draw the shape
+        public UIElement Convert()
+        {
+            return new Line()
+            {
+                X1 = startPoint.X,
+                Y1 = startPoint.Y,
+                X2 = endPoint.X,
+                Y2 = endPoint.Y,
+                StrokeThickness = 1,
+                Stroke = new SolidColorBrush(Colors.Red)
+            }; ;
+        }
+    }
+
+}
